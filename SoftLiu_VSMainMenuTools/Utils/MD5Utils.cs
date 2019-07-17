24262eddab5d597ccd85f06a679ad0e862e3bafd
SunHailang <hai-lang.sun@ubisoft.com>
2019-07-17 11:30:44 +0800

    add event manager update

---
+++ SoftLiu_VSMainMenuTools/Utils/MD5Utils.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftLiu_VSMainMenuTools.Utils
+{
+    public class MD5Utils
+    {
+        /// <summary>
+        /// 获取一个 MD5 字符串
+        /// </summary>
+        /// <param name="key">字符串类型</param>
+        /// <returns></returns>
+        public static string GetMD5String(string key)
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return null;
+            }
+            MD5 md5 = MD5.Create();
+            byte[] bts = md5.ComputeHash(Encoding.UTF8.GetBytes(key));
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bts.Length; i++)
+            {
+                sb.Append(bts[i].ToString("X2"));
+                if (i < bts.Length - 1)
+                {
+                    sb.Append(":");
+                }
+            }
+            return sb.ToString();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key">二进制流类型</param>
+        /// <returns>MD5 字符串</returns>
+        public static string GetMD5String(byte[] key)
+        {
+            if (key != null)
+            {
+                return null;
+            }
+            MD5 md5 = MD5.Create();
+            byte[] bts = md5.ComputeHash(key);
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < bts.Length; i++)
+            {
+                sb.Append(bts[i].ToString("X2"));
+                if (i < bts.Length - 1)
+                {
+                    sb.Append(":");
+                }
+            }
+            return sb.ToString();
+        }
+    }
+}
---
+++ SoftLiu_VSMainMenuTools/Utils/RandomUtils.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoftLiu_VSMainMenuTools.Utils
+{
+    public class RandomUtils
+    {
+        /// <summary>
+        /// 获取 一个随机 int 值
+        /// 左闭右开
+        /// </summary>
+        /// <returns>A 32-bit signed integer greater than or equal to zero and less than System.Int32.MaxValue.</returns>
+        public static int Next()
+        {
+            Random rd = new Random(m_RandomSeed);
+            return rd.Next();
+        }
+        /// <summary>
+        /// 获取一个指定随机种子的 int 随机值
+        /// 左闭右开
+        /// </summary>
+        /// <param name="seed">随机种子</param>
+        /// <returns>A 32-bit signed integer greater than or equal to zero and less than System.Int32.MaxValue.</returns>
+        public static int Next(int seed)
+        {
+            Random rd = new Random(seed);
+            return rd.Next();
+        }
+        /// <summary>
+        /// 获取一个指定范围的 int 随机值
+        /// 左闭右开
+        /// </summary>
+        /// <param name="minValue">最小值（包含）</param>
+        /// <param name="maxValue">最大值（不包含）</param>
+        /// <returns>the range of return values includes minValue but not maxValue.</returns>
+        public static int Next(int minValue, int maxValue)
+        {
+            Random rd = new Random(m_RandomSeed);
+            return rd.Next(minValue, maxValue);
+        }
+        /// <summary>
+        /// 获取一个指定范围的 int 随机值
+        /// 左闭右开
+        /// </summary>
+        /// <param name="seed">随机种子</param>
+        /// <param name="minValue">最小值（包含）</param>
+        /// <param name="maxValue">最大值（不包含）</param>
+        /// <returns>the range of return values includes minValue but not maxValue.</returns>
+        public static int Next(int seed, int minValue, int maxValue)
+        {
+            Random rd = new Random(seed);
+            return rd.Next(minValue, maxValue);
+        }
+        /// <summary>
+        /// 获取一个随机种子
+        /// </summary>
+        public static int m_RandomSeed
+        {
+            get
+            {
+                byte[] bytes = new byte[4];
+                RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
+                rng.GetBytes(bytes);
+                return BitConverter.ToInt32(bytes, 0);
+            }
+        }
+    }
+}
