dceee534537247caa6471ebd0091818de4dad28d
HaiLangSun <hai-lang.sun@ubisoft.com>
2020-08-26 16:06:52 +0800

    update

--- SoftLiu_VSMainMenuTools/SplashLoader.cs
+++ SoftLiu_VSMainMenuTools/SplashLoader.cs
@@ -44,27 +44,62 @@ namespace SoftLiu_VSMainMenuTools
 
 
         //在你的button事件中写下如下代码就行
-        private void RefreshX();
-      {
-      while(true)
-    {
-      int n = 0;
-      if (!InternetGetConnectedState(out n,0))
-      {
-      MessageBox.Show("网络处于未连接状态");
+        private void RefreshX()
+        {
+            while (true)
+            {
+                int n = 0;
+                if (!InternetGetConnectedState(out n, 0))
+                {
+                    MessageBox.Show("网络处于未连接状态");
 
-      }
-}
+                }
+            }
+        }
 
-private void SplashLoader_Load(object sender, EventArgs e)
-{
+        private void SplashLoader_Load(object sender, EventArgs e)
+        {
 
-    CheckForIllegalCrossThreadCalls = false;
-    m_progressBarSplash = new ProgressBarExtension(progressBarSplash);
-    m_progressBarSplash.Value = 0;
-    m_progressBarSplash.onChanged += (value) =>
-    {
-        if (value >= 100)
+            CheckForIllegalCrossThreadCalls = false;
+            m_progressBarSplash = new ProgressBarExtension(progressBarSplash);
+            m_progressBarSplash.Value = 0;
+            m_progressBarSplash.onChanged += (value) =>
+            {
+                if (value >= 100)
+                {
+                    Thread th = new Thread(() =>
+                    {
+                        while (true)
+                        {
+                            try
+                            {
+                                Thread.Sleep(800);
+                                m_parent.MainMenuForm_Init();
+                                close = false;
+                                this.Close();
+                                break;
+                            }
+                            catch (Exception msg)
+                            {
+                                Console.WriteLine("onChanged : " + msg.Message);
+                            }
+                        }
+                    });
+                    th.Start();
+                }
+            };
+            //获取UI线程同步上下文
+            App.Instance.InitSyncContext(SynchronizationContext.Current);
+            m_progressBarSplash.Value = 30;
+
+            // read csv file
+            Localization.Instance.Init();
+
+            //m_progressBarSplash.Value = 100;
+            init();
+        }
+
+        private void init()
         {
             Thread th = new Thread(() =>
             {
@@ -72,70 +107,36 @@ private void SplashLoader_Load(object sender, EventArgs e)
                 {
                     try
                     {
-                        Thread.Sleep(800);
-                        m_parent.MainMenuForm_Init();
-                        close = false;
-                        this.Close();
-                        break;
+                        Thread.Sleep(100);
+                        if (m_progressBarSplash.Value >= 100)
+                        {
+                            break;
+                        }
+                        m_progressBarSplash.Value++;
+
                     }
                     catch (Exception msg)
                     {
-                        Console.WriteLine("onChanged : " + msg.Message);
+                        Console.WriteLine("SplashLoader init : " + msg.Message);
                     }
                 }
             });
             th.Start();
         }
-    };
-    //获取UI线程同步上下文
-    App.Instance.InitSyncContext(SynchronizationContext.Current);
-    m_progressBarSplash.Value = 30;
 
-    // read csv file
-    Localization.Instance.Init();
-
-    //m_progressBarSplash.Value = 100;
-    init();
-}
-
-private void init()
-{
-    Thread th = new Thread(() =>
-    {
-        while (true)
+        private void SplashLoader_FormClosing(object sender, FormClosingEventArgs e)
         {
-            try
-            {
-                Thread.Sleep(100);
-                if (m_progressBarSplash.Value >= 100)
-                {
-                    break;
-                }
-                m_progressBarSplash.Value++;
-
-            }
-            catch (Exception msg)
-            {
-                Console.WriteLine("SplashLoader init : " + msg.Message);
-            }
+            e.Cancel = close;
         }
-    });
-    th.Start();
-}
 
-private void SplashLoader_FormClosing(object sender, FormClosingEventArgs e)
-{
-    e.Cancel = close;
-}
+        private void progressBarSplash_BindingContextChanged(object sender, EventArgs e)
+        {
 
-private void progressBarSplash_BindingContextChanged(object sender, EventArgs e)
-{
+        }
 
-}
+        private void progressBarSplash_ChangeUICues(object sender, UICuesEventArgs e)
+        {
 
-private void progressBarSplash_ChangeUICues(object sender, UICuesEventArgs e)
-{
-
-}
+        }
     }
 }
